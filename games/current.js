320c8e670042f7c6ee9ff8afbe77ce09cfd208e7
fitoussino-tech <fitoussi.no@gmail.com>
2026-07-29 10:38:06 +0200

    Update current.js

--- games/current.js
+++ games/current.js
@@ -1,5 +1,5 @@
 window.CURRENT_GAME = {
-  path:  'games/week-01-word-ladder.html',  // which file to load
+  path:  'word-ladder.html',  // which file to load
   title: 'WORD LADDER',                      // name shown on the site
   emoji: '',                                // icon
   week:  1                                    // week number
