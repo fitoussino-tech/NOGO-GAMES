41cad630a20b358fe5921ec8af37c5233dd66b02
fitoussino-tech <fitoussi.no@gmail.com>
2026-07-31 12:06:36 +0200

    Update current.js

--- games/current.js
+++ games/current.js
@@ -13,7 +13,8 @@ window.GAMES = [
   { title: 'MOO.F.O', emoji: '🛸', path: 'games/moofo.html'}, 
   { title: 'HOSED', emoji: '🚒', path: 'games/hosed.html'},
   { title: 'CLASSROOM', emoji: '🧑‍🦯', path: 'games/classroom.html'},
-   { title: 'SCALPEL', emoji: '😷', path: 'games/scalpel.html'},
+  { title: 'SCALPEL', emoji: '😷', path: 'games/scalpel.html'},
   { title: 'LIFE SIMULATOR', emoji: '👶', path: 'games/lifesimulator.html'} 
+  { title: 'HONEY CLICKER', emoji: '🐝', path: 'games/honeyclicker.html'}
 ];
 window.CURRENT_INDEX = 3;   // 0 = Word Ladder is this week's free game
