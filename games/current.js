874569dd9e41c6ec60c96fcc1930ae69eeb2a416
fitoussino-tech <fitoussi.no@gmail.com>
2026-07-31 16:44:50 +0200

    Update current.js

--- games/current.js
+++ games/current.js
@@ -15,6 +15,7 @@ window.GAMES = [
   { title: 'CLASSROOM', emoji: '🧑‍🦯', path: 'games/classroom.html'},
   { title: 'SCALPEL', emoji: '😷', path: 'games/scalpel.html'},
   { title: 'LIFE SIMULATOR', emoji: '👶', path: 'games/lifesimulator.html'}, 
-  { title: 'HONEY CLICKER', emoji: '🐝', path: 'games/honeyclicker.html'}
+  { title: 'HONEY CLICKER', emoji: '🐝', path: 'games/honeyclicker.html'},
+  {title: 'HONEY CLICKER', emoji: '⛳️', path: 'games/teesmash.html'}
 ];
 window.CURRENT_INDEX = 3;   // 0 = Word Ladder is this week's free game
